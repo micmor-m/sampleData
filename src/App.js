f75ba2f37cecdf6a4888b8bef8928f8cc7e8a757
mic <micmor.m@gmail.com>
2021-07-18 15:15:37 -0400

    add container

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
 import CommonCard from "components/CommonCard";
 import "./styles/variables.scss";
+import "./styles/container.scss";
 
 function App() {
   return (
     <main className="layout">
       {/* Add Responsive container component here. Render the card components inside of that component.  */}
-      <div>
-        Replace me with a responsive container component to hold the group of
-        cards!
+      <section className="container">
         <CommonCard />
-      </div>
+      </section>
     </main>
   );
 }
